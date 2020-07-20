5f12f4b4651d109b0a50aa8741826853ce535797
Simone Fiorentini <simonefiorentini1@gmail.com>
2020-07-20 23:11:49 +0300

    added temporary pagination

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FetchJobs from "./components/useFetchJobs";
 import Loading from "./components/Loading";
 import Job from "./components/Job";
 import { Container } from "react-bootstrap";
+import JobPages from "./components/Pagination";
 
 function App() {
   const [params, setParams] = useState({});
@@ -14,13 +15,16 @@ function App() {
   return (
     <div className="App">
       <Container className="container my-5">
+        <h1 className="mb-5">GitHub Jobs</h1>
+        <JobPages page={page} setPage={setPage} hasNextPage={true} />
         {loading && <Loading />}
-        {error && <h1>There was an error. Try Refreshing</h1>}
+        {error && <h4 className="error">There was an error. Try Refreshing</h4>}
         {!loading &&
           !error &&
           jobs.map((job) => {
             return <Job key={job.id} job={job} />;
           })}
+        <JobPages page={page} setPage={setPage} />
       </Container>
     </div>
   );
